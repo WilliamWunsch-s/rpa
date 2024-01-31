475b688aebdf9ff76c38b0e72c884c335665cf8d
WilliamWunsch-s <79519857+WilliamWunsch-s@users.noreply.github.com>
2024-01-31 09:34:22 -0300

    Update getDocuments.js

--- getDocuments.js
+++ getDocuments.js
@@ -2,21 +2,16 @@ const puppeteer = require('puppeteer');
 // const express = require('express');
 // const bodyParser = require('body-parser');
 const fs = require('fs').promises;
+const path = require('path');
+const pdf2pic = require("pdf2pic");
 
 // const app = express();
 
 async function getDocuments(username, password, number) {
     const browser = await puppeteer.launch({
-        headless: false,
-        userDataDir: './user_data',
-        defaultViewport: null,
-        args: [
-            `--download.default_directory=${process.cwd()}/C:/Users/Usuário/Downloads`
-        ]
+        headless: false
     });
     const page = await browser.newPage();
-
-
     await page.goto('https://app.botconversa.com.br');
     await page.reload({ waitUntil: ["networkidle0", "domcontentloaded"] });
     await new Promise(resolve => setTimeout(resolve, 2000));
@@ -42,33 +37,45 @@ async function getDocuments(username, password, number) {
     for (let chatMessage of chatMessages) {
         const documentElement = await chatMessage.$('.document-message');
         if (documentElement) {
+            const descriptionElement = await documentElement.$('.document-message__description');
+            const description = await page.evaluate(descriptionElement => descriptionElement.textContent, descriptionElement);
             const downloadElement = await documentElement.$('.document-message__loader');
+            const dateElement = await documentElement.$('span.date');
             if (downloadElement) {
-                const descriptionElement = await documentElement.$('.document-message__description');
-                const fileName = await page.evaluate(element => element.textContent, descriptionElement);
                 await downloadElement.click();
-                await page.waitForNavigation({ waitUntil: 'networkidle0' });
-
-                const filePath = path.join('C:/Users/Usuário/Downloads', fileName);
-                try {
-                    const fileData = await fs.readFile(filePath);
-                    const binaryData = Buffer.from(fileData).toString('binary');
-
-                    documents.push(binaryData);
-
-                    await fs.unlink(filePath);
-                } catch (error) {
-                    console.error(`Error reading or converting file: ${error}`);
-                }
+                await new Promise(resolve => setTimeout(resolve, 2000));
+                const date = await page.evaluate((dateElement) => dateElement.innerText, dateElement);
+                documents.push({name:description, date: date})
             }
         }
     }
     await browser.close();
     console.log(documents);
     return documents;
-
 }
-getDocuments("lucas@fique.online", "jrwL9Za2b*T_vt@", "11992960111")
+
+async function processFiles() {
+    const files = await getDocuments("lucas@fique.online", "jrwL9Za2b*T_vt@", "11992960111");
+
+    let documentsInBinary = [];
+    files.forEach(element => {
+        let nome = element.name;
+        let arquivo = path.join('C:', 'Users', 'Usuário', 'Downloads', `${nome}`);
+        fs.readFile(arquivo , 'utf8')
+        .then(data => {
+            const binaryData = Buffer.from(data).toString('binary');
+            const jsonData = JSON.stringify(binaryData);
+            documentsInBinary.push({archive: jsonData, date: element.date});
+        })
+        .catch(err => {
+            throw err;
+        });   
+    });
+
+    console.log(documentsInBinary);
+}
+
+processFiles();
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
 // app.post('/getDocuments', async (req, res) => {
